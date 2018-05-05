94413fefce9d6b8cb4b7195a150a002f5a9b42fe
logonrm <logonrm@fiap.com.br>
2018-05-05 12:18:22 -0300

    Initial commit

---
+++ app/src/main/java/br/com/satoruchannel/meusjogosfavoritos/ListaActivity.kt
@@ -0,0 +1,12 @@
+package br.com.satoruchannel.meusjogosfavoritos
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista)
+    }
+}
